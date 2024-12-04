295d038249d67c07d649a80b22c91d84e37114cf
Navodit1603 <navoditmaheshwari@gmail.com>
2024-12-03 23:29:25 -0800

    test

--- madlib.py
+++ madlib.py
@@ -1,13 +1,24 @@
 import nltk
 from nltk import word_tokenize, pos_tag
-
-# run only on the first time
-# -------------------------------------------------
-# nltk.download('punkt_tab')
-# nltk.download('averaged_perceptron_tagger_eng')
+import re
 
 text = "I love eating pizza?"
-tokens = word_tokenize(text)
-tagged = pos_tag(tokens)
 
-print(tagged)
+def ntlk_pos_tagger(text):
+    # run only on the first time
+    # -------------------------------------------------
+    # nltk.download('punkt_tab')
+    # nltk.download('averaged_perceptron_tagger_eng')
+
+    tokens = word_tokenize(text)
+    tagged = pos_tag(tokens)
+
+    print(tagged)
+
+def pos_tagger(text):
+    tokens = re.findall(r'\w+', text)
+    pos_tags = []
+    print(tokens)
+
+
+ntlk_pos_tagger(text)
