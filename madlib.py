0baef14d5462fd9878b124d9a953a5df5bdc5011
Navodit1603 <navoditmaheshwari@gmail.com>
2024-12-03 18:40:38 -0800

    got pos tag with nltk

--- madlib.py
+++ madlib.py
@@ -1,1 +1,13 @@
-# Madlib
+import nltk
+from nltk import word_tokenize, pos_tag
+
+# run only on the first time
+# -------------------------------------------------
+# nltk.download('punkt_tab')
+# nltk.download('averaged_perceptron_tagger_eng')
+
+text = "I love eating pizza?"
+tokens = word_tokenize(text)
+tagged = pos_tag(tokens)
+
+print(tagged)
