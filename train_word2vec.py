2245463e00dfee0cdf3acf2e00e6944ca5ea98a3
fjordimm <fjordimm@gmail.com>
2024-12-10 14:39:58 -0800

    made the final script for making the word embedding

--- train_word2vec.py
+++ train_word2vec.py
@@ -1,8 +1,8 @@
 
 from io import TextIOWrapper
+import pathlib
 from typing import Optional
 from gensim.models import Word2Vec
-from nltk.corpus import brown, movie_reviews, treebank
 import nltk.tokenize
 
 
@@ -11,28 +11,25 @@ OUTPUT_PATH = './data_extracted/word2vec/wikipedia_embedding.model'
 
 
 def main():
-    print('Starting word2vec...')
-    # make_word_embedding_for_wikipedia_sentences()
-    wikipedia_sentences_iterator = WikipediaSentencesIterator()
-    with wikipedia_sentences_iterator as wsi:
-        # bruh = iter(wsi)
-        # print(next(bruh))
-        # print(next(bruh))
-        # print(next(bruh))
-        # print(next(bruh))
-        # print(next(bruh))
-        word_embedding = Word2Vec(vector_size=100, window=5, min_count=1, workers=4)
-        word_embedding.build_vocab(iter(wsi))
-        word_embedding.save(OUTPUT_PATH)
-    print('...Done')
+    pathlib.Path('./data_extracted/word2vec').mkdir(parents=True, exist_ok=True)
+
+    print('This may take a while...')
     print()
 
-    word_embedding = Word2Vec.load(OUTPUT_PATH)
-    print(word_embedding.wv.most_similar('the', topn=10))
+    print('Starting word2vec for Wikipedia Sentences...')
+    wikipedia_sentences_iterator = WikipediaSentencesIterator(max_lines=1000000)
+    with wikipedia_sentences_iterator as wsi:
+        word_embedding = Word2Vec(iter(wsi), vector_size=100, window=5, min_count=1, workers=4)
+        word_embedding.save(OUTPUT_PATH)
+    print('Finished word2vec for Wikipedia Sentences.')
+    print()
+
+    # word_embedding = Word2Vec.load(OUTPUT_PATH)
+    # print(word_embedding.wv.most_similar('the', topn=10))
 
 
 class WikipediaSentencesIterator:
-    def __init__(self, max_lines: int = 500):
+    def __init__(self, max_lines: int = 1000):
         self._max_lines: int = max_lines
         self._in_file: Optional[TextIOWrapper] = None
         self._current_line: Optional[str] = ''
@@ -62,22 +59,6 @@ class WikipediaSentencesIterator:
                     self._current_line = self._in_file.readline()
                     self._current_line_num += 1
                     return nltk.tokenize.word_tokenize(self._current_line)
-
-def make_word_embedding_for_wikipedia_sentences(max_lines=10000):
-    with open(WIKIPEDIA_SENTENCES_PATH, 'r') as in_file:
-        lines = []
-        line = in_file.readline()
-        for i in range(0, max_lines):
-            if not line:
-                break
-
-            lines.append(nltk.tokenize.word_tokenize(line))
-            line = in_file.readline()
-        
-        print('..done reading lines...')
-        
-        word_embedding = Word2Vec(sentences=lines, vector_size=100, window=5, min_count=1, workers=4)
-        word_embedding.save(OUTPUT_PATH)
 
 
 if __name__ == '__main__':
